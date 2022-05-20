cf0e49b4a42ad7980f4c9189caddf5d6f1002817
Tazaev <tonymurtazaev@gmail.com>
2022-05-20 13:50:26 -0400

    added some java

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+
+// variables/dependiences
+// function for button in card
+//when "click" button fetch var url
+//return to document.location.replace(url)
+//
+//
+//display function in unordered list
